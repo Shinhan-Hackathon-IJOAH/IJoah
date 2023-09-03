89cabaf20379aa182fdcaec2e61fe9ab7fd78af7
유승민 <byuri1356@gmail.com>
2023-09-03 22:07:44 +0900

    add swagger config class (#26)

---
+++ backend/src/main/java/com/shinhan/shbhack/ijoa/common/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.shinhan.shbhack.ijoa.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket restAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.shinhan.shbhack.ijoa.api.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("아이조아 API")
+                .version("1.0.0")
+                .description("아이 친화적인 자산 관리 플랫폼 ijoa api 입니다.")
+                .build();
+    }
+}
+
