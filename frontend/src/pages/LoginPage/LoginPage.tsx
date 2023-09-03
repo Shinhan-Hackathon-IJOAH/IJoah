d6f6ed92934fc2b791fbad2c7676011161ed282d
이지헌 <jiheon_114@naver.com>
2023-09-03 14:53:43 +0900

    feat: 라우터 기능 구현

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const LoginPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div>로그인페이지</div>;
 };
 
 export default LoginPage;
