09829f70ec6de38a50a680d424e187bbe3304a28
이지헌 <jiheon_114@naver.com>
2023-09-05 00:53:31 +0900

    feat: 회원가입 페이지 뼈대 구현(#40)

---
+++ frontend/src/components/SignUp.tsx/ParentSignUp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ParentSignUp = () => {
+    return (
+        <div>
+            <div>부모 회원가입 페이지</div>
+        </div>
+    );
+};
+
+export default ParentSignUp;
