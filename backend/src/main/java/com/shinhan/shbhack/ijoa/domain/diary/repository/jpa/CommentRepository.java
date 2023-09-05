8cc85ceaa7830ca0f66e9d49f5ec6869b13dbfd7
김하영 <bnk159hair@naver.com>
2023-09-05 17:35:39 +0900

    feat: diary JPA 중복 삭제(#31)

--- backend/src/main/java/com/shinhan/shbhack/ijoa/domain/diary/repository/jpa/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shinhan.shbhack.ijoa.domain.diary.repository.jpa;
-
-import com.shinhan.shbhack.ijoa.domain.diary.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-
-}
