07c305886e1b6c34b73645583de844dfb6462796
유승민 <byuri1356@gmail.com>
2023-09-06 14:25:14 +0900

    feat:securit y s비활성화 & yml 파일 분할 (#61)

--- backend/src/main/java/com/shinhan/shbhack/ijoa/api/controller/member/MemberController.java
+++ backend/src/main/java/com/shinhan/shbhack/ijoa/api/controller/member/MemberController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/members")
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class MemberController {
 
