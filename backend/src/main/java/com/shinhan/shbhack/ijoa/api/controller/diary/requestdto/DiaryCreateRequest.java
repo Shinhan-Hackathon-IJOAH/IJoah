944583738ca63e9090d7784c4b282c98e4c3c616
김하영 <bnk159hair@naver.com>
2023-09-06 09:01:28 +0900

    feat: security 관련 주석, baseEntity 어노테이션 추가, 작성 테스트 확인 및 리스트는 아직..

--- backend/src/main/java/com/shinhan/shbhack/ijoa/api/controller/diary/requestdto/DiaryCreateRequest.java
+++ backend/src/main/java/com/shinhan/shbhack/ijoa/api/controller/diary/requestdto/DiaryCreateRequest.java
@@ -2,14 +2,26 @@ package com.shinhan.shbhack.ijoa.api.controller.diary.requestdto;
 
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
 public class DiaryCreateRequest {
+    private String memberId;
+    private String title;
+    private String emotion;
+    private String content;
+    private LocalDate date;
+    private List<MultipartFile> photo;
 
 
 }
--- backend/src/main/java/com/shinhan/shbhack/ijoa/domain/diary/repository/datajpa/DiaryRepository.java
+++ backend/src/main/java/com/shinhan/shbhack/ijoa/domain/diary/repository/datajpa/DiaryRepository.java
@@ -3,6 +3,8 @@ package com.shinhan.shbhack.ijoa.domain.diary.repository.datajpa;
 import com.shinhan.shbhack.ijoa.domain.diary.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
 }
