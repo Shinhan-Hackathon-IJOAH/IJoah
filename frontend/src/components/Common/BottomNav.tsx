4a1f5d94c258ab8e3974a5a4e381046d499b869d
이지헌 <jiheon_114@naver.com>
2023-09-08 03:00:22 +0900

    feat:사이드바 기능 구현(#72)

--- frontend/src/components/Common/BottomNav.tsx
+++ frontend/src/components/Common/BottomNav.tsx
@@ -1,17 +1,75 @@
-import React from 'react';
-import {BottomNavContent,HomeImg,AlarmImg,MenuImg} from './BottomNavStyles'
+import React, { useState } from "react";
+import {
+  BottomNavContent,
+  HomeImg,
+  AlarmImg,
+  MenuImg,
+} from "./BottomNavStyles";
 
+import { Icon, Menu, Sidebar, Segment, Header, Image } from "semantic-ui-react";
 
 const BottomNav = () => {
+  const [visible, setVisible] = useState(false);
+
+  const handleMenuClick = () => {
+    setVisible(!visible);
+  };
   return (
-    <BottomNavContent>
-      <AlarmImg/>
-      <HomeImg/>
-      <MenuImg/>
-    </BottomNavContent>
+    <div>
+      {/* 사이드바 */}
+      <Sidebar
+        as={Menu}
+        color="orange"
+        animation="overlay"
+        direction="right"
+        icon="labeled"
+        inverted
+        onHide={() => setVisible(false)}
+        vertical
+        visible={visible}
+        width="thin"
+      >
+        <div className="">
+          <div>
+            <Menu.Item as="a">
+              <Icon name="home" />
+              Home
+            </Menu.Item>
+            <Menu.Item as="a">
+              <Icon name="gamepad" />
+              Games
+            </Menu.Item>
+            <Menu.Item as="a">
+              <Icon name="camera" />
+              Channels
+            </Menu.Item>
+          </div>
+          <div>
+            <Menu.Item onClick={() => setVisible(false)} as="a">
+              <Icon name="close" />
+              닫기
+            </Menu.Item>
+          </div>
+        </div>
+      </Sidebar>
+      <Sidebar.Pusher dimmed={visible}>
+        <BottomNavContent>
+          <AlarmImg />
+          <HomeImg />
+          <MenuImg onClick={handleMenuClick} />
+        </BottomNavContent>
+      </Sidebar.Pusher>
+    </div>
   );
 };
 
-
-
 export default BottomNav;
+
+// export default function App() {
+//   return (
+//     <div>
+//       <BottomNav />
+
+//     </div>
+//   );
+// }
