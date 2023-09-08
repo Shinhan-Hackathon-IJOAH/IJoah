eaaf2452622d3f85052cb7e599070e035cd35945
김성준 <gkxm0443@naver.com>
2023-09-09 01:17:32 +0900

    feat: 부모 메인페이지 axios틀 구현(#78)

--- frontend/src/pages/ParentPage/ParentGiveMissionPage/ParentGiveMissionPage.tsx
+++ frontend/src/pages/ParentPage/ParentGiveMissionPage/ParentGiveMissionPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import GiveMissionDetail from '../../../components/ParentGiveMission/GiveMissionDetail';
-import GiveMissionSetting from '../../../components/ParentGiveMission/GiveMissionSetting';
 import {useNavigate} from 'react-router-dom';
 import BottomNav from '../../../components/Common/BottomNav'
 
@@ -8,9 +7,7 @@ const ParentGiveMissionPage = () => {
     const navigate = useNavigate();
     return (
         <div>
-            <GiveMissionSetting/>
             <GiveMissionDetail />
-            <button>미션 등록</button>
             <button onClick={()=>{
                 navigate(-1);
             }}>뒤로 가기</button>
--- frontend/src/pages/ParentPage/ParentRegisterPage/ParentRegisterPage.tsx
+++ frontend/src/pages/ParentPage/ParentRegisterPage/ParentRegisterPage.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React,{useState} from 'react';
+import axios from 'axios';
+
 
 const ParentRegisterPage = () => {
+    const [childaccount, setChildAccount] = useState('');
+    const [childid, setChildId] = useState('');
+
+    const registerChild=()=>{
+        axios
+            .post('',{childaccount,childid},{
+                headers: {
+                    Authorization: `Bearer`,
+                    },
+            })
+            .then((response) =>{
+                console.log(response)
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+    }
     return (
         <div>
             아이계좌 정보 입력
-            <input type="text" />
+            <input type="number" value="계좌를 입력해주세요" onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setChildAccount(event.target.value)} />
             아이 아이디 입력
-            <input type="text" />
+            <input type="text" value="아이 아이디를 입력해주세요" onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setChildId(event.target.value)} />
+
+            <button onClick={()=>{registerChild()}}>입력완료</button>
         </div>
     );
 };
