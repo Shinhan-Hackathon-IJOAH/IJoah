172d463233d4d75f069b9d2342dba9e989dc6f2b
이지헌 <jiheon_114@naver.com>
2023-09-09 23:50:27 +0900

    feat: 아이페이지 중 거래내역 조회 페이지 기능 구현(#76)

--- frontend/src/pages/ChildPage/ChildTradePage/ChildTradePage.tsx
+++ frontend/src/pages/ChildPage/ChildTradePage/ChildTradePage.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import ChildTrade from "../../../components/ChildTrade/ChildTradeList";
 const ChildTradePage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <ChildTrade />
+    </div>
+  );
 };
 
 export default ChildTradePage;
