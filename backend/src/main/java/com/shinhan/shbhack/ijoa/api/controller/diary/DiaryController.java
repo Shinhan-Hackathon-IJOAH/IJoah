7b3a1406374cb440490a521d2da0fe8a9741fd58
김하영 <bnk159hair@naver.com>
2023-09-10 01:51:04 +0900

    feat: 사진 표시 API 추가

--- backend/src/main/java/com/shinhan/shbhack/ijoa/api/controller/diary/DiaryController.java
+++ backend/src/main/java/com/shinhan/shbhack/ijoa/api/controller/diary/DiaryController.java
@@ -7,11 +7,19 @@ import com.shinhan.shbhack.ijoa.api.service.diary.dto.response.DiaryCalenderResp
 import com.shinhan.shbhack.ijoa.api.service.diary.dto.response.DiaryDetailResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Slf4j
@@ -20,6 +28,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DiaryController {
     private final DiaryService diaryService;
+    @Value("${file.dir}")
+    public String basicPath;
 //    @PostMapping("")
 //    public ResponseEntity<?> writeDiary(@RequestBody DiaryCreateRequest diaryCreateRequest){
 //
@@ -66,5 +76,23 @@ public class DiaryController {
     public ResponseEntity<?> shareDiary(@PathVariable Long diaryId){
         diaryService.shareDiary(diaryId);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/image/{path}")
+    public ResponseEntity<?> showImage(@PathVariable String path){
+        log.info("===================");
+        Resource resource = new FileSystemResource(basicPath+path);
+        if(!resource.exists()){
+            return new ResponseEntity<Resource>(HttpStatus.NOT_FOUND);
+        }
+        HttpHeaders header = new HttpHeaders();
+        Path filePath = null;
+        try{
+            filePath = Paths.get(basicPath+path);
+            header.add("Content-Type", Files.probeContentType(filePath));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<Resource>(resource, header, HttpStatus.OK);
     }
 }
