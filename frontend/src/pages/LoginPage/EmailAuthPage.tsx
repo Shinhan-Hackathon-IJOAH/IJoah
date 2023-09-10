d44db3bb30d3109af80441cbdcbaaa538aa04a7a
이지헌 <jiheon_114@naver.com>
2023-09-10 19:27:29 +0900

    feat:이메일 인증,검사 페이지 기능 구현(#76)

--- frontend/src/pages/LoginPage/EmailAuthPage.tsx
+++ frontend/src/pages/LoginPage/EmailAuthPage.tsx
@@ -6,22 +6,21 @@ import {
   CardFooter,
   Typography,
   Button,
-  Input
+  Input,
 } from "@material-tailwind/react";
 import axios from "axios";
 import EmailAuthSend from "./../../components/Login/EmailAuthSend";
 import EmailAuthCheck from "./../../components/Login/EmailAuthCheck";
 import { useSignUpStore } from "../../store/SignUpStore";
 const EmailAuthPage = () => {
-    const { isSendEmail, setIsSendEmail } = useSignUpStore();
-    
+  const { isSendEmail, setIsSendEmail } = useSignUpStore();
 
-
-  return <div
-  >
-    <EmailAuthSend></EmailAuthSend>
-   {isSendEmail? (<EmailAuthCheck/>) : <EmailAuthSend/> } 
-</div>;
+  return (
+    <div>
+      {/* 이메일을 보냈으면 검증 페이지 보여주기, 아니면 이메일 입력 페이지 보여주기 */}
+      {isSendEmail ? <EmailAuthCheck /> : <EmailAuthSend />}
+    </div>
+  );
 };
 
 export default EmailAuthPage;
