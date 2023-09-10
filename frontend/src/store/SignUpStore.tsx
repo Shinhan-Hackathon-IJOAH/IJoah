ef826efa1869e38e08e1a7f7bbe5d555f3e5b755
이지헌 <jiheon_114@naver.com>
2023-09-10 14:00:48 +0900

    feat:회원가입 기능 구현(#76)

---
+++ frontend/src/store/SignUpStore.tsx
@@ -0,0 +1,37 @@
+// /src/zustand/store.ts
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+
+// store 업데이트 했으면 쿠키 날려야함.
+interface SignUpStore {
+  isSendEmail: boolean;
+  setIsSendEmail: (isSendEmail: boolean) => void;
+  realAuthCode: any;
+    setRealAuthCode: (realAuthCode: any) => void;
+    isAuthEmail: boolean;
+    setIsAuthEmail: (isAuthEmail: boolean) => void;
+    signUpEmail: string;
+    setSignUpEmail: (signUpEmail: string) => void;
+
+
+  
+}
+
+export const useSignUpStore = create<SignUpStore>()(
+  devtools(
+    persist(
+    (set) => ({
+      isSendEmail: false,
+        setIsSendEmail: (payload) => set({ isSendEmail: payload }),
+        realAuthCode: "",
+        setRealAuthCode: (payload) => set({ realAuthCode: payload }),
+        isAuthEmail: false,
+        setIsAuthEmail: (payload) => set({ isAuthEmail: payload }),
+        signUpEmail: "",
+        setSignUpEmail: (payload) => set({ signUpEmail: payload }),
+    }),
+    {
+      name: "singUp-storage", // persist key
+    }
+  ))
+);
