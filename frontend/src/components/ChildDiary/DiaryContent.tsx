5ee3efc9df788cd5aca3d73cce54153703983582
이지헌 <jiheon_114@naver.com>
2023-09-10 19:25:56 +0900

    feat: 아이 페이지 중 일기 조회 기능 구현(#76)

--- frontend/src/components/ChildDiary/DiaryContent.tsx
+++ frontend/src/components/ChildDiary/DiaryContent.tsx
@@ -1,14 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, Typography } from "@material-tailwind/react";
+import { Avatar, Typography, Carousel } from "@material-tailwind/react";
 // import {Diary} from './DiaryContentStyles'
 import axios from "axios";
 interface DiaryContentProps {
   selectdate: string;
   diaryId: string;
 }
+interface Diary {
+  diaryId: number;
+  title: string;
+  content: string;
+  emotion: string;
+  diary_date: string;
+  images: {
+    storeFileName: string;
+    diaryImageId: string;
+    uploadFileName: string;
+  }[];
+
+  record: string;
+}
 
 const DiaryContent: React.FC<DiaryContentProps> = ({ selectdate, diaryId }) => {
-  const [diary, setDiary] = useState<any>([]);
+  const [diary, setDiary] = useState<Diary>();
 
   // diaryId 값이 바뀔 때마다 axios get 요청하는 함수
   useEffect(() => {
@@ -28,22 +42,22 @@ const DiaryContent: React.FC<DiaryContentProps> = ({ selectdate, diaryId }) => {
     <div>
       {/* 일기 쓴 날짜 */}
       <div>
-        <Typography variant="h3">날짜 : {diary.diary_date}</Typography>
+        <Typography variant="h3">날짜 : {diary?.diary_date}</Typography>
       </div>
 
       {/* 제목 */}
       <div>
-        <Typography variant="h3">제목 : {diary.title}</Typography>
+        <Typography variant="h3">제목 : {diary?.title}</Typography>
       </div>
       {/* 내용 */}
       <div>
-        <Typography variant="h3">내용 : {diary.content}</Typography>
+        <Typography variant="h3">내용 : {diary?.content}</Typography>
       </div>
 
       {/* 일기 쓴 날짜의 기분 날씨 */}
       <div className="mt-10">
         <div className="text-2xl text-center font-['HSYuji-Regular']">
-          {diary.diary_date} 의 기분 날씨는 어땠나요 ?
+          {diary?.diary_date} 의 기분 날씨는 어땠나요 ?
         </div>
         <div className="flex mt-5 gap-4 justify-center">
           <div>
@@ -52,7 +66,7 @@ const DiaryContent: React.FC<DiaryContentProps> = ({ selectdate, diaryId }) => {
               alt="avatar"
               src="/weather/sunny.png"
               className={`border p-1 opacity-30 border-[#F8A70C] shadow-md shadow-[#F8A70C] ring-2 ring-[#F8A70C] ${
-                diary.emotion === "sunny"
+                diary?.emotion === "sunny"
                   ? "ring-[red] ring-2 !opacity-100 border-[red]"
                   : ""
               }`}
@@ -64,7 +78,7 @@ const DiaryContent: React.FC<DiaryContentProps> = ({ selectdate, diaryId }) => {
               alt="avatar"
               src="/weather/sunrise.png"
               className={`border p-1 opacity-30 border-[#F8A70C] shadow-md shadow-[#F8A70C] ring-2 ring-[#F8A70C] ${
-                diary.emotion === "sunrise"
+                diary?.emotion === "sunrise"
                   ? "ring-[red] ring-2 !opacity-100 border-[red]"
                   : ""
               }`}
@@ -76,7 +90,7 @@ const DiaryContent: React.FC<DiaryContentProps> = ({ selectdate, diaryId }) => {
               alt="avatar"
               src="/weather/drop.png"
               className={`border p-1 border-[#F8A70C] opacity-30 shadow-md shadow-[#F8A70C] ring-2 ring-[#F8A70C] ${
-                diary.emotion === "drop"
+                diary?.emotion === "drop"
                   ? "ring-[red]  !opacity-100 ring-2 border-[red]"
                   : ""
               }`}
@@ -88,7 +102,7 @@ const DiaryContent: React.FC<DiaryContentProps> = ({ selectdate, diaryId }) => {
               alt="avatar"
               src="/weather/wind.png"
               className={`border p-1 border-[#F8A70C] shadow-md opacity-30 shadow-[#F8A70C] ring-2 ring-[#F8A70C] ${
-                diary.emotion === "wind"
+                diary?.emotion === "wind"
                   ? "ring-[red] ring-2 !opacity-100 border-[red]"
                   : ""
               }`}
@@ -100,7 +114,7 @@ const DiaryContent: React.FC<DiaryContentProps> = ({ selectdate, diaryId }) => {
               alt="avatar"
               src="/weather/thunder.png"
               className={`border p-1 border-[#F8A70C] shadow-md opacity-30 shadow-[#F8A70C] ring-2 ring-[#F8A70C] ${
-                diary.emotion === "thunder"
+                diary?.emotion === "thunder"
                   ? "ring-[red] ring-2 !opacity-100 border-[red]"
                   : ""
               }`}
@@ -108,15 +122,34 @@ const DiaryContent: React.FC<DiaryContentProps> = ({ selectdate, diaryId }) => {
           </div>
         </div>
       </div>
-      {/* <div>날짜:{selectdate}</div> */}
-      {/* {diaryId, title, content, emotion, diary_date, images, record} */}
-      <div>아이디 :{diary.diaryId}</div>
-      <div>제목 :{diary.title}</div>
-      <div>내용 :{diary.content}</div>
-      <div>기분 :{diary.emotion}</div>
-      <div>날짜 :{diary.diary_date}</div>
-      {/* <div>사진 :{diary.images[0]}</div> */}
-      {/* <div>음성 :{diary.record}</div> */}
+
+      {/* 일기 쓴 날짜의 사진 */}
+      <Carousel
+        className="rounded-xl"
+        navigation={({ setActiveIndex, activeIndex, length }) => (
+          <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+            {new Array(length).fill("").map((_, i) => (
+              <span
+                key={i}
+                className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                  activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+                }`}
+                onClick={() => setActiveIndex(i)}
+              />
+            ))}
+          </div>
+        )}
+      >
+        {diary?.images.map((image, index) => (
+          // 각 이미지 객체에서 storeFileName에 접근
+          <img
+            className="h-96 w-96 w-full rounded-lg object-contain object-center shadow-xl shadow-blue-gray-900/50"
+            key={index}
+            src={`https://ijoah01.duckdns.org/api/diaries/image/${image.storeFileName}`}
+            alt={`Selected ${index}`}
+          />
+        ))}
+      </Carousel>
     </div>
   );
 };
