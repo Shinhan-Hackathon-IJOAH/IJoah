33583831192ffe5b1b2d2731f6837557a56cfd2b
김하영 <bnk159hair@naver.com>
2023-09-17 01:20:09 +0900

    fix : 등록된 계좌인지 확인하는 부분 문제

--- backend/src/main/java/com/shinhan/shbhack/ijoa/domain/member/repository/datajpa/MemberRepository.java
+++ backend/src/main/java/com/shinhan/shbhack/ijoa/domain/member/repository/datajpa/MemberRepository.java
@@ -14,4 +14,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     Optional<Member> findByAccount(Account account);
+
+    boolean existsMemberByAccount_AccountNumber(String accountNumber);
 }
